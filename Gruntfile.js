4f09e0fecb0a72b63954afb2f8601afe21542720
cs <bartus.csongor@gmail.com>
2014-11-11 21:44:39 +0200

    Assemble cannot include atomic partials

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,42 @@
-
 'use strict';
 
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    
+    
+    // HTML
+    // Generate HTML files from Handlebars templates
     assemble: {
       // Task-level options
       options: {
-        assets: 'assets'
+        assets: 'assets',
+        partials: ['components/**/*.hbs']
       },
+      // Target-level options
       site: {
-        // Target-level options
-        files: [ { 
+        files: [{ 
           expand: true, 
           cwd: 'components/pages/', 
           src: ['**/*.hbs'], 
-          dest: 'public' }
-        ]
+          dest: 'public' 
+        }]
       }
     },
 
     // Before generating any new files,
     // remove any previously-created files.
     clean: {
-      all: ['public/**/*.{html}']
-    }
+      all: ['public/**/*']
+    },
+    
+    watch: {
+      html: {
+        files: ['components/**/*.hbs'],
+        tasks: 'html'
+      }
+    },  
   });
 
   // Load npm plugins to provide necessary tasks.
@@ -33,9 +44,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-verb');
   grunt.loadNpmTasks('grunt-newer' );
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task to be run.
-  grunt.registerTask('default', ['clean', 'newer:assemble']);
+  grunt.registerTask('default', ['clean', 'assemble']);
+  
+  // All other tasks
+  grunt.registerTask('html', ['assemble']);
 };
 
 
